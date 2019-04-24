76996604631e7f039a22bbb746ae54f7dab9b327
Nine Bro <shinjisrg@gmail.com>
2019-04-24 13:36:48 +0800

    Add the best practices and the most clever solution from Code Wars.

--- codewars_predict_age.js
+++ codewars_predict_age.js
@@ -9,4 +9,15 @@ function predictAge(...ages){
   console.log(Math.sqrt(sum) / 2 | 0); // trun  answer to Integer
 }
 
+// The Best Practices:
+// function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+//   let arr = [age1, age2, age3, age4, age5, age6, age7, age8]
+//   return Math.floor(Math.sqrt(arr.map(a => a * a).reduce((b,c) => b + c)) / 2)
+// }
+
+// The most clever answer:
+// const predictAge = (...ages) => Math.hypot(...ages) / 2 | 0;
+
+
+
 predictAge(65, 60, 75, 55, 60, 63, 64, 45)
