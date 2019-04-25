726c0b3652b2c7e9e1a721a82d549ffde79cb624
Nine Bro <shinjisrg@gmail.com>
2019-04-25 10:43:22 +0800

    Practice with 2 more katas on Codewars.

--- codewars_find_the_stray.js
+++ codewars_find_the_stray.js
@@ -1,19 +1,28 @@
+// Find the stray number
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+// Complete the method which accepts such an array, and returns that single different number.
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
 function stray(numbers) {
-  for(let i = 0; i < numbers.length; i ++){
-    if (numbers[i] !== numbers[(i + 1)] && numbers[i] !== numbers[i - 1]){
-      return numbers[i]
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== numbers[i + 1] && numbers[i] !== numbers[i - 1]) {
+      return numbers[i];
     }
   }
 }
 
 // anothor practice:
 function stray(numbers) {
-  let j = numbers.length - 1
-  for(let i = 0; i < numbers.length; i ++){
-    if(numbers[i] !== numbers[j]){
+  let j = numbers.length - 1;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== numbers[j]) {
       return numbers[i] === numbers[i + 1] ? numbers[j] : numbers[i];
-    i += 1
-    j -= 1
+      i += 1;
+      j -= 1;
     }
   }
 }
--- codewars_sum_of_the_first_nth_term_of_series.js
+++ codewars_sum_of_the_first_nth_term_of_series.js
@@ -1,13 +1,23 @@
-function SeriesSum(n)
-{
-  let sum = 0.00;
-    for(let i = 0; i < n; i ++){
-      sum += (1 / (1 + i * 3));
-    }
-  console.log(sum.toFixed(2))
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+// If the given value is 0 then it should return 0.00
+// You will only be given Natural Numbers as arguments.
+// Examples:
+// SeriesSum(1) => 1 = "1.00"
+// SeriesSum(2) => 1 + 1/4 = "1.25"
+// SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
+
+function SeriesSum(n) {
+  let sum = 0.0;
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + i * 3);
+  }
+  console.log(sum.toFixed(2));
 }
 
-SeriesSum(1) // 1.00
-SeriesSum(2) // 1.25
-SeriesSum(3) // 1.39
-SeriesSum(4) // 1.49
+SeriesSum(1); // 1.00
+SeriesSum(2); // 1.25
+SeriesSum(3); // 1.39
+SeriesSum(4); // 1.49
