f1f6dd07255c9a574572ad5b4049c482edf24882
Nine Bro <shinjisrg@gmail.com>
2019-04-29 16:12:22 +0800

    Second version of home work page, there's double summit issue of input button that related to 'change' event of img-url input.

--- 190426_homework_page_Hurry_Poster/homework_page_190426.js
+++ 190426_homework_page_Hurry_Poster/homework_page_190426.js
@@ -1,11 +1,43 @@
 $(document).ready(function(evt) {
+  // 移除上傳的圖片
+  function removeImg(){
+    console.log('!!');
+      $('.img-fluid').click(function(evt) {
+      // $('.img-fluid').bind("click", function(evt) {
+        console.log($(this));
+        // alert(1);
+        // $('#img-fluid').fadeOut("slow");
+        $(this).remove();
+      });
+  }
+  removeImg();
   // Insert imgs to the first
+  
   $('#img-url').on('change', function(evt){
-    let imgHref = $(this).val();
-    console.log(imgHref);
-    // 這一段 src value 無法正常 separate from alt
+    console.log('a');
+      $('.img-section').prepend(`<img class="img-fluid rounded" src="${$('#img-url').val()}" alt="Oops! Can't find the image.">`);
+      $(this).val('');
+      removeImg()
+    });
+    
+  $('.btn').click(function(evt) {
+    console.log('b');
+    console.log($('#img-url').val());
+    let imgHref = $('#img-url').val();
     $('.img-section').prepend(`<img class="img-fluid rounded" src="${imgHref}" alt="Oops! Can't find the image.">`);
+    $('#img-url').val('');
+    removeImg()
   });
+
+
+  // 快速加入圖片
+  $('li').click(function(evt) {
+    $('.img-section').prepend(`<img class="img-fluid rounded" src="${$(this).text()}" alt="Oops! Can't find the image.">`);
+    removeImg()
+  });
+  
+
+
 
   // Special effect of input text area.
   // 輸入時內容時，將說明文字往上移動
